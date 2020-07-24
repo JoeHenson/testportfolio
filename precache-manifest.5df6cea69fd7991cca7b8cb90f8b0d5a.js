544e6c414b9e5f4e288f21cb686649cb5f30a09a
Joe Henson <62956527+JoeHenson@users.noreply.github.com>
2020-07-24 14:21:09 -0500

    Updates

---
+++ precache-manifest.5df6cea69fd7991cca7b8cb90f8b0d5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd79d5bb1dce4fc26c97b2cacc5993d",
+    "url": "/testportfolio/index.html"
+  },
+  {
+    "revision": "152662de0894d21e002f",
+    "url": "/testportfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "64b7352b1d16ee85eaf2",
+    "url": "/testportfolio/static/js/2.bb386828.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/testportfolio/static/js/2.bb386828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152662de0894d21e002f",
+    "url": "/testportfolio/static/js/main.911db4fe.chunk.js"
+  },
+  {
+    "revision": "9dae0caf55434704f80b",
+    "url": "/testportfolio/static/js/runtime-main.afcc9464.js"
+  }
+]);
